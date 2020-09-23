892f66888f9b1a70b58862d25813546f83cd3429
badaev <badaev777@yandex.ua>
2020-09-24 01:13:57 +0300

    shopify-fast-fix script

--- base-part/shopify-fast-fix.js
+++ base-part/shopify-fast-fix.js
@@ -5,25 +5,36 @@ const shopify = new Shopify({
   apiKey: "f39d45d049010045efb8e6999cbf826d",
   password: "5a93738fd403ecca695b35fcedc68c26",
 });
+
 const productsList = [];
 let params = { since_id: 0, limit: 250 };
 let stop = true;
+let limitRemaining = 40;
+let counter = 0;
+
 shopify.product.count().then(async (count) => {
+  console.log('total count -->', count);
   do {
-    // await sleep(500);
+    await sleep(500);
     const products = await shopify.product.list(params);
-    // console.log(products.length);
-    console.log(products.length);
     productsList.push(...products);
-    // console.log(products[products.length - 1].id);
+    counter += products.length;
+    console.log('page next -->', products.length);
+    console.log('counter -->',counter);
     if (products.length) {
       for (const prod of products) {
         for (const variant of prod.variants) {
           if (variant.inventory_management) {
-            await sleep(500);
+            if(limitRemaining < 10){
+              console.log('shopifyCallLimitRemaning', limitRemaining)
+              await sleep(2000);
+            }
+            console.log('prod.id',prod.id);
+            console.log('variant.id',variant.id);
             await shopify.productVariant.update(variant.id, {
               inventory_management: null,
             });
+            console.log('variant.id complete',variant.id);
           }
         }
       }
@@ -32,7 +43,7 @@ shopify.product.count().then(async (count) => {
       stop = false;
     }
   } while (stop);
-  console.log("asdad", productsList.length);
+  console.log("done -->", productsList.length);
 });
 
 function sleep(ms) {
@@ -40,3 +51,7 @@ function sleep(ms) {
     setTimeout(resolve, ms);
   });
 }
+
+shopify.on('callLimits', (limits) => {
+  limitRemaining = limits.remaining;
+});
